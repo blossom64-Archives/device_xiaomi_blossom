1cc62c5e2a4f948fecf83442ae9c2bdd19b6b3cc
root <root@kali.kai.org>
2025-09-16 15:11:30 -0500

    blossom: lights: drop minimum light value from 19 to 6

--- lights/Light.cpp
+++ lights/Light.cpp
@@ -88,7 +88,7 @@ static inline uint32_t scaleBrightness(uint32_t brightness, uint32_t maxBrightne
     }
 
     // T25 - reduce min brightness to 13
-    return (brightness - 1) * (maxBrightness - 19) / (0xFF - 1) + 19;
+    return (brightness - 1) * (maxBrightness - 6) / (0xFF - 1) + 6;
 }
 
 static inline uint32_t getScaledBrightness(const HwLightState& state, uint32_t maxBrightness) {
